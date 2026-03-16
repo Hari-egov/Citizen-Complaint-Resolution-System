b256b0576ec9abdcd5235ae496a49500bca15079
mersh101 <meraolfeye6@gmail.com>
2026-03-16 09:05:56 +0300

    some fix

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/pgr/src/configs/CreateComplaintConfig.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/pgr/src/configs/CreateComplaintConfig.js
@@ -39,7 +39,7 @@ export const CreateComplaintConfig = {
                 error: "CORE_COMMON_REQUIRED_ERRMSG",
                 validation: {
                   required: true,
-                  pattern: /^(?!.*[ _-]{2})(?!^[\s_-])(?!.*[\s_-]$)(?=^[A-Za-z][A-Za-z0-9 _\-\(\)]{4,29}$)^.*$/,
+                  pattern: /^(?!.*[ _-]{2})(?!^[\s_-])(?!.*[\s_-]$)(?=^[A-Za-z\u1200-\u137F][A-Za-z0-9\u1200-\u137F _\-\(\)]{4,29}$)^.*$/
                 }
               },
             },
